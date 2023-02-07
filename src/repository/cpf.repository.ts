590257f4f4b3674ea3e9bdfb477345a5d9f84b32
erickmarx <erickmarx5@gmail.com>
2023-02-07 16:14:35 -0300

    update: response from methods of cpf repository

--- src/repository/cpf.repository.ts
+++ src/repository/cpf.repository.ts
@@ -3,29 +3,34 @@ import { cpf } from '@prisma/client';
 import { PrismaService } from '~/infra/prisma/prisma.service';
 
 @Injectable()
-export class CPFRepository {
+export class CpfRepository {
   constructor(private prismaService: PrismaService) {}
 
-  async get(cpf: string): Promise<cpf> {
-    return await this.prismaService.cpf.findFirstOrThrow({
+  async get(cpf: string): Promise<Pick<cpf, 'cpf' | 'createdAt'>> {
+    return await this.prismaService.cpf.findFirst({
       where: { cpf, deletedAt: null },
+      select: { cpf: true, createdAt: true },
     });
   }
 
-  async getAll(): Promise<cpf[]> {
+  async getAll(): Promise<Pick<cpf, 'cpf' | 'createdAt'>[]> {
     return await this.prismaService.cpf.findMany({
       where: { deletedAt: null },
     });
   }
 
-  async create(cpf: string): Promise<cpf> {
-    return await this.prismaService.cpf.create({ data: { cpf } });
+  async create(cpf: string): Promise<Pick<cpf, 'cpf' | 'createdAt'>> {
+    return await this.prismaService.cpf.create({
+      data: { cpf },
+      select: { cpf: true, createdAt: true },
+    });
   }
 
-  async delete(cpf: string): Promise<cpf> {
+  async delete(cpf: string): Promise<Pick<cpf, 'cpf' | 'createdAt'>> {
     return await this.prismaService.cpf.update({
       where: { cpf_deletedAt: { cpf, deletedAt: null } },
       data: { deletedAt: new Date() },
+      select: { cpf: true, createdAt: true },
     });
   }
 }
