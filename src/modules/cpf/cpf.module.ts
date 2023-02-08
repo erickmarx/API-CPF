5ab99e5eb504a325a7923811595e23565ccc6afe
erickmarx <erickmarx5@gmail.com>
2023-02-08 13:25:40 -0300

    add: providers on cpf module

--- src/modules/cpf/cpf.module.ts
+++ src/modules/cpf/cpf.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { CpfController } from './cpf.controller';
 import { RepositoryModule } from '~/repository/repository.module';
 import { VerifyIfCpfRegisteredService } from './services/verify-if-registered.service';
+import { DeleteCpfService } from './services/delete.service';
+import { GetAllCpfService } from './services/get-all.service';
+import { RegisterCpfService } from './services/register.service';
 
 @Module({
   imports: [RepositoryModule],
   controllers: [CpfController],
-  providers: [VerifyIfCpfRegisteredService],
+  providers: [
+    VerifyIfCpfRegisteredService,
+    GetAllCpfService,
+    RegisterCpfService,
+    DeleteCpfService,
+  ],
 })
 export class CpfModule {}
